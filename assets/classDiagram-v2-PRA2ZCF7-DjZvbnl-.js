ae3ad1c702fe2876744241350680a60194c6f160
yudi-corgi <45381436+yudi-corgi@users.noreply.github.com>
2025-06-04 07:34:28 +0000

    feat: Modified link

---
+++ assets/classDiagram-v2-PRA2ZCF7-DjZvbnl-.js
@@ -0,0 +1,1 @@
+import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-4ELtt3ME.js";import"./chunk-IO347I67-CdBqZLnm.js";import{m as o}from"./mermaid.esm.min-D9ydQqzO.js";import"./app-BG5kq-F4.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
