85dda7df63f79fd9ee230c3e892d0a35edd27449
yudi-corgi <45381436+yudi-corgi@users.noreply.github.com>
2025-06-11 07:17:38 +0000

    chore: Modified icon.

---
+++ assets/classDiagram-MKYM2BOE-Dz-BgLlw.js
@@ -0,0 +1,1 @@
+import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-BbXVrLsM.js";import"./chunk-IO347I67-B-oRN-Mp.js";import{m as o}from"./mermaid.esm.min-DyIok1oY.js";import"./app-BcJUZ_3z.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
