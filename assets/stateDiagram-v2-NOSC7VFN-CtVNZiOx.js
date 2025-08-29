4bdfe620b961e95296f03a1fa79766a7b9d8825e
yudi-corgi <45381436+yudi-corgi@users.noreply.github.com>
2025-08-29 03:43:28 +0000

    docs: style.

---
+++ assets/stateDiagram-v2-NOSC7VFN-CtVNZiOx.js
@@ -0,0 +1,1 @@
+import{m as e,s,j as t,x as a}from"./chunk-D7JRX7UM-OCTtlv8W.js";import"./chunk-IO347I67-F5CQlxq6.js";import{m}from"./mermaid.esm.min-UQtGiLL5.js";import"./app-C7XbtJhB.js";var b={parser:t,get db(){return new a(2)},renderer:s,styles:e,init:m(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{b as diagram};
