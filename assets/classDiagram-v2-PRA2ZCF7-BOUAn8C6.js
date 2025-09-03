72a0ba694c1187b8c93088bc1953aea913519dc8
yudi-corgi <45381436+yudi-corgi@users.noreply.github.com>
2025-09-03 06:15:58 +0000

    docs: style.

--- assets/classDiagram-v2-PRA2ZCF7-BOUAn8C6.js
+++
@@ -1,1 +0,0 @@
-import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-C2GhV_0w.js";import"./chunk-IO347I67-F5CQlxq6.js";import{m as o}from"./mermaid.esm.min-UQtGiLL5.js";import"./app-C7XbtJhB.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
