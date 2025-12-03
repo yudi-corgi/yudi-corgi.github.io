5cd6775c14b700e044df3481fd27b59ddc5bb861
yudi-corgi <45381436+yudi-corgi@users.noreply.github.com>
2025-12-03 03:23:40 +0000

    chore: optimized sidebar directory indentation.

--- assets/classDiagram-v2-PRA2ZCF7-BT4RqaOE.js
+++
@@ -1,1 +0,0 @@
-import{w as a,Y as s,k as t,u as e}from"./chunk-SHLHLJRN-Bl6xcvfc.js";import"./chunk-IO347I67-UHD8GWjO.js";import{m as o}from"./mermaid.esm.min-z36XaKK8.js";import"./app-B1JkHOU2.js";var p={parser:t,get db(){return new e},renderer:s,styles:a,init:o(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
